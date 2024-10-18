f34c47234cae06a7a5da9fea777adeedc921443f
Alanbiju255 <105338945+Alanbiju255@users.noreply.github.com>
2024-10-18 08:33:56 +0530

    Update Atm.c

--- Atm.c
+++ Atm.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 
-#include<stdio.h>
 int main() {
     int a, choice, accountbalance = 1000, deposit, withdraw;
     char name[] = "Alan";
@@ -22,26 +22,38 @@ int main() {
         return 0;
     }
 
-    printf("1. For Deposit\n");
-    printf("2. For Withdraw\n");
-    printf("Please select your option:\n ");
-    scanf("%d", &choice);
-    switch (choice) {
-        case 1:
-            printf("Enter the amount to deposit: ");
-            scanf("%d", &deposit);
-            accountbalance += deposit;
-            printf("Account balance: %d\n", accountbalance);
-            break;
-        case 2:
-            printf("Enter the amount to withdraw: ");
-            scanf("%d", &withdraw);
-            accountbalance -= withdraw;
-            printf("Account balance: %d\n", accountbalance);
-            break;
-        default:
-            printf("Invalid option. Please try again.\n");
-            break;
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Deposit\n");
+        printf("2. Withdraw\n");
+        printf("3. Exit\n");
+        printf("Please select your option: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the amount to deposit: ");
+                scanf("%d", &deposit);
+                accountbalance += deposit;
+                printf("Account balance: %d\n", accountbalance);
+                break;
+            case 2:
+                printf("Enter the amount to withdraw: ");
+                scanf("%d", &withdraw);
+                if (withdraw > accountbalance) {
+                    printf("Insufficient balance!\n");
+                } else {
+                    accountbalance -= withdraw;
+                    printf("Account balance: %d\n", accountbalance);
+                }
+                break;
+            case 3:
+                printf("Thank you for using our service. Goodbye!\n");
+                return 0;
+            default:
+                printf("Invalid option. Please try again.\n");
+                break;
+        }
     }
 
     return 0;
